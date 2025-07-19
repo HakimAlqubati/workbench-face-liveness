ef0b6aabfcbe17e560315610503beaa7d1e1e099
HakimAlqubati <hakimahmed123321@gmail.com>
2025-07-19 04:36:41 +0300

    Initial commit: React face liveness with AWS Rekognition integration

---
+++ src/aws-exports.js
@@ -0,0 +1,6 @@
+const awsConfig = {
+  aws_project_region: 'us-east-1',         // غيّرها عند الحاجة
+  aws_cognito_identity_pool_id: 'ap-southeast-1:66b6a0f0-3d6b-457c-aedc-cb0da3cd3417',        // غيّرها عند الربط الفعلي
+  aws_cognito_region: 'us-east-1',
+};
+export default awsConfig;
